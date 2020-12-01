b0f5b0c184c451c69389b16902f0280b74cda322
nimi2838 <bbongsky11@naver.com>
2020-12-01 16:31:51 +0900

    finish

--- bhn/index.js
+++ bhn/index.js
@@ -1,47 +1,48 @@
 function SliderBox1__init() {
-    $('.slider-box-1 > .slick').slick({
-      arrows:true,
-      dots:true,
-      autoplay: true,
-      autoplaySpeed: 5000,
-      pauseOnHover:false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      prevArrow:".slider-box-1 > .arrows > .btn-arrows-left",
-      nextArrow:".slider-box-1 > .arrows > .btn-arrows-right"
-    });
-  }
-  
-  $(function() {
-    SliderBox1__init();
+  $('.slider-box-1 > .slick').slick({
+    arrows: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".slider-box-1 > .arrows > .btn-arrows-left",
+    nextArrow: ".slider-box-1 > .arrows > .btn-arrows-right"
   });
-  
-  function SliderBox2__init() {
-    $('.slider-box-2 > .slick').slick({
-      autoplay: true,
-      autoplaySpeed: 5000,
-      pauseOnHover:false,
-      slidesToShow: 3,
-      slidesToScroll: 1
-    });
-  }
-  
-  $(function() {
-    SliderBox2__init();
+}
+
+$(function () {
+  SliderBox1__init();
+});
+
+function SliderBox2__init() {
+  $('.slider-box-2 > .slick').slick({
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    slidesToShow: 3,
+    slidesToScroll: 1
   });
-  
-  
-  
-  function SliderBox3__init() {
-    $('.slider-box-3 > .slick').slick({
-      autoplay: true,
-      autoplaySpeed: 3000,
-      pauseOnHover:false,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    });
-  }
-  
-  $(function() {
-    SliderBox3__init();
+}
+
+$(function () {
+  SliderBox2__init();
+});
+
+
+
+function SliderBox3__init() {
+  $('.slider-box-3 > .slick').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false
   });
+}
+
+$(function () {
+  SliderBox3__init();
+});
